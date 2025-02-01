33fa44d208215b28a8bf03c3fcdce39c1d01f0d7
Abdoubzn29 <122210227+Abdoubzn29@users.noreply.github.com>
2025-02-01 05:27:27 +0100

    ImageCompression.java

--- ImageCompression.java
+++ ImageCompression.java
@@ -5,8 +5,10 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
@@ -15,20 +17,92 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.io.IOUtils;
+
 public class ImageCompression {
     public static class PngToJpegMapper
-            extends Mapper<Object, BytesWritable, NullWritable, BytesWritable> {
+            extends Mapper<Text, BytesWritable, Text, BytesWritable> {
 
-        public void map(Object key, BytesWritable value, Context context)
+        public void map(Text key, BytesWritable value, Context context)
                 throws InterruptedException {
             try {
                 ByteArrayInputStream bais = new ByteArrayInputStream(value.getBytes());
                 BufferedImage pngImage = ImageIO.read(bais);
                 ByteArrayOutputStream baos = new ByteArrayOutputStream();
                 ImageIO.write(pngImage, "jpeg", baos);
-                context.write(NullWritable.get(), new BytesWritable(baos.toByteArray()));
+                context.write(key, new BytesWritable(baos.toByteArray()));
             } catch (Exception e) {
                 // Handle exceptions
+            }
+        }
+    }
+
+    // Pass-through reducer
+    public static class ImageCompressionReducer
+            extends Reducer<Text, BytesWritable, Text, BytesWritable> {
+        public void reduce(Text key, Iterable<BytesWritable> values, Context context)
+                throws IOException, InterruptedException {
+            for (BytesWritable val : values) {
+                // Write each compressed image
+                context.write(key, val);
+            }
+        }
+    }
+
+    public static class WholeFileInputFormat extends FileInputFormat<Text, BytesWritable> {
+        @Override
+        protected boolean isSplitable(org.apache.hadoop.fs.FileSystem fs, Path file) {
+            return false;
+        }
+        @Override
+        public RecordReader<Text, BytesWritable> createRecordReader(
+                InputSplit split, TaskAttemptContext context) {
+            return new WholeFileRecordReader();
+        }
+        static class WholeFileRecordReader extends RecordReader<Text, BytesWritable> {
+            private boolean processed = false;
+            private Text key = new Text();
+            private BytesWritable value = new BytesWritable();
+            private FSDataInputStream fis;
+            private long fileLength;
+            @Override
+            public void initialize(InputSplit split, TaskAttemptContext context) throws IOException {
+                Path file = ((org.apache.hadoop.mapreduce.lib.input.FileSplit) split).getPath();
+                org.apache.hadoop.fs.FileSystem fs = file.getFileSystem(context.getConfiguration());
+                fis = fs.open(file);
+                fileLength = fs.getFileStatus(file).getLen();
+                key.set(file.toString());
+            }
+            @Override
+            public boolean nextKeyValue() throws IOException {
+                if (!processed) {
+                    byte[] contents = new byte[(int) fileLength];
+                    IOUtils.readFully(fis, contents, 0, contents.length);
+                    value.set(contents, 0, contents.length);
+                    processed = true;
+                    return true;
+                }
+                return false;
+            }
+            @Override
+            public Text getCurrentKey() {
+                return key;
+            }
+            @Override
+            public BytesWritable getCurrentValue() {
+                return value;
+            }
+            @Override
+            public float getProgress() {
+                return processed ? 1.0f : 0.0f;
+            }
+            @Override
+            public void close() throws IOException {
+                if (fis != null) fis.close();
             }
         }
     }
@@ -38,9 +112,11 @@ public class ImageCompression {
         Job job = Job.getInstance(conf, "PNG to JPEG Compression");
         job.setJarByClass(ImageCompression.class);
         job.setMapperClass(PngToJpegMapper.class);
-        job.setNumReduceTasks(0);
-        job.setOutputKeyClass(NullWritable.class);
+        job.setReducerClass(ImageCompressionReducer.class);
+        job.setNumReduceTasks(1);
+        job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(BytesWritable.class);
+        job.setInputFormatClass(WholeFileInputFormat.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
